63d70aac7ef9a4b64f26a9abf0de29afdf1dc933
Michelle <jhmichellekim@gmail.com>
2016-12-28 12:45:52 -0800

    initial commit for a backup version of package rubber-ducky

---
+++ spec/rubber-ducky3-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RubberDucky3View from '../lib/rubber-ducky3-view';
+
+describe('RubberDucky3View', () => {
+  it('has one valid test', () => {
+    expect('coding').toBe('fun');
+  });
+});
